2add1d235aafc8d7044ef93f4fbf1cbd53bbb349
João Victor Graciano Belfort de Andrade <belfort.joao216@gmail.com>
2021-05-14 18:40:14 -0300

    novos exercicios
    criei uma nova pasta apra exercicios e novos 3 exercicios do 2 ao 4

---
+++ codes/Ex002.py
@@ -0,0 +1,2 @@
+x = input('qual o seu nome? ')
+print('Olá,', x,'é um prazer te conhecer')
---
+++ codes/Ex003.py
@@ -0,0 +1,5 @@
+print('welcome to the calculator')
+x = int(input('write a number: '))
+y = int(input('write a second number: '))
+j = x + y
+print('the some {} of and {} is  equal to: {}' .format(x, y, j))
